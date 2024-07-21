930eab9327c6722f8397dac896de6372186aaa04
guizmo <martinezguillaume@users.noreply.github.com>
2024-07-21 06:34:22 +0200

    add bug

---
+++ modules/expo-amazon-chime/ios/ExpoAmazonChimeModule.swift
@@ -0,0 +1,11 @@
+import ExpoModulesCore
+import AmazonChimeSDK
+
+public class ExpoAmazonChimeModule: Module {
+  public func definition() -> ModuleDefinition {
+    Name("ExpoAmazonChime")
+    
+    OnCreate {
+    }
+  }
+}
